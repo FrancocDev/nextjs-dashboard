64a9dc25e5f7a677cd89a13fb3c284f429cfb7f9
Franco Carrara <frankko212@gmail.com>
2023-12-07 18:54:54 -0300

    Add metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,12 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import { Metadata } from 'next'; 
+
+export const meta: Metadata = {
+  title: 'Dashboard',
+  description: 'NExt.js Course Dashboard',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
